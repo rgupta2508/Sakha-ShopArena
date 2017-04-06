801b751fff2e9aa641151ee028c8ac0fb436cf4a
rgupta2508 <r.gupta2508@gmail.com>
2017-04-06 18:17:05 +0530

    product detail

---
+++ Shop-Arena/src/main/java/com/sakha/shoparena/webapi/controller/ProductDetailController.java
@@ -0,0 +1,43 @@
+package com.sakha.shoparena.webapi.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sakha.shoparena.webapi.dao.impl.ProductDetailImpl;
+import com.sakha.shoparena.webapi.entity.Product;
+import com.sakha.shoparena.webapi.service.ProductDetailsService;
+import com.sakha.shoparena.webapi.service.impl.ProductDetailsServiceImpl;
+
+@RestController
+@RequestMapping(value = "/shoparena")
+public class ProductDetailController {
+
+	
+	ProductDetailsServiceImpl productDetailsServiceImpl=new ProductDetailsServiceImpl();
+
+	@RequestMapping(value = "/test", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String getTestData(@RequestBody String testString)
+	{ 
+		String result="hello word"+testString;
+
+		return result;
+	}
+
+
+	@RequestMapping(value = "/getproductlist", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Product> getProductList(@RequestBody String testString)
+	{ 
+		List<Product> productList=productDetailsServiceImpl.getProductListService();
+
+		return productList;
+	}
+	
+}
