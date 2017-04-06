7fa125bde9a03b32e24d565143db8784851141fe
rgupta2508 <r.gupta2508@gmail.com>
2017-04-06 17:45:43 +0530

    Initial commit

--- Shop-Arena/src/main/java/com/sakha/shoparena/webapi/AppStart.java
+++ Shop-Arena/src/main/java/com/sakha/shoparena/webapi/AppStart.java
@@ -1,9 +1,15 @@
 package com.sakha.shoparena.webapi;
 
+import java.util.HashSet;
+import java.util.Set;
 
-
+import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.sakha.shoparena.webapi.entity.Product;
+import com.sakha.shoparena.webapi.entity.User;
+import com.sakha.shoparena.webapi.util.HibernateUtil;
 
 @SpringBootApplication
 public class AppStart {
@@ -12,6 +18,57 @@ public class AppStart {
 		
 		SpringApplication.run(AppStart.class,args);
 		
-	}
+		
+//		   System.out.println("Hibernate many to many (Annotation)");
+//			Session session = HibernateUtil.getSessionFactory().openSession();
+//
+//			session.beginTransaction();
+//
+//			User stock = new User();
+//			
+//		        stock.setFirstName("7a521j11");
+//		        stock.setUserName("PADINIka11");
+//
+//		        Product category1 = new Product("CONSUMER", "CONSUMER COMPANY");
+//		        Product category2 = new Product("INVESTMENT", "INVESTMENT COMPANY");
+//
+//		        Set<Product> categories = new HashSet<Product>();
+//		        categories.add(category1);
+//		        categories.add(category2);
+//
+//		        stock.setProducts(categories);
+//
+//		        session.save(stock);
+//
+//			session.getTransaction().commit();
+//			System.out.println("Done");
+		
+/*
+	        System.out.println("Hibernate many to many (Annotation)");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+
+			User user = new User();
+	        //stock.setStockCode("7052");
+	        //stock.setStockName("PADINI");
+
+	        Product product = new Product();
+	        Product product2 = new Product();
+	        product.setBrandName("hp");
+	        product2.setBrandName("dell");
+
+	        Set<Product> productSet = new HashSet<Product>();
+	        productSet.add(product);
+	        productSet.add(product2);
+
+	        user.setProduct(productSet);
+
+	        session.save(user);
+
+		session.getTransaction().commit();
+		System.out.println("Done");*/
+		}
+		
 
 }
--- Shop-Arena/src/main/java/com/sakha/shoparena/webapi/controller/ProfileController.java
+++ Shop-Arena/src/main/java/com/sakha/shoparena/webapi/controller/ProfileController.java
@@ -1,5 +1,9 @@
 package com.sakha.shoparena.webapi.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,22 +11,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sakha.shoparena.webapi.dao.impl.ProductDetailImpl;
+import com.sakha.shoparena.webapi.entity.Product;
+import com.sakha.shoparena.webapi.service.ProductDetailsService;
+import com.sakha.shoparena.webapi.service.impl.ProductDetailsServiceImpl;
+
 @RestController
 @RequestMapping(value = "/shoparena")
 public class ProfileController {
 
-
 	
+	ProductDetailsServiceImpl productDetailsServiceImpl=new ProductDetailsServiceImpl();
 
 	@RequestMapping(value = "/test", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody String getTestData(@RequestBody String testString)
-	{
-		//String result="hello word"+testString;
-
+	{ 
+		String result="hello word"+testString;
 
 		return result;
 	}
 
-	
+
+	@RequestMapping(value = "/getproductlist", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Product> getProductList(@RequestBody String testString)
+	{ 
+		List<Product> productList=productDetailsServiceImpl.getProductListService();
+
+		for (Product product:productList){
+			System.out.println(product.getName());
+		}
+		return productList;
+	}
 	
 }
