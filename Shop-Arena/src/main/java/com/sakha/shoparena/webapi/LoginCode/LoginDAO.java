f3db243ebd7c4a244f45e2332482cad7cc175621
ImranKabir123 <imran.kabir@sakhatech.com>
2017-04-07 18:38:38 +0530

    deleted

--- Shop-Arena/src/main/java/com/sakha/shoparena/webapi/LoginCode/LoginDAO.java
+++
@@ -1,6 +0,0 @@
-package main.java.com.sakha.shoparena.webapi.LoginCode;
-//import com.sakha.model.*;
-
-public interface LoginDAO{    
-       public boolean checkLogin(String userName, String userPassword);
-}
--- Shop-Arena/src/main/java/com/sakha/shoparena/webapi/service/LoginService.java
+++
@@ -1,6 +0,0 @@
-package main.java.com.sakha.shoparena.webapi.service;
-//import com.sakha.model.*;
-
-public interface LoginService{    
-       public boolean checkLogin(String userName, String userPassword);
-}
